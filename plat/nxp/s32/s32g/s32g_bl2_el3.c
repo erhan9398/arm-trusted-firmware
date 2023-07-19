ae289c59168db859b9b49c0b18949df21e1e0ec6
erhan cengiz <erhan9398ua@gmail.com>
2023-07-19 19:55:49 +0300

    test print added

--- plat/nxp/s32/s32g/s32g_bl2_el3.c
+++ plat/nxp/s32/s32g/s32g_bl2_el3.c
@@ -87,19 +87,22 @@ void bl2_el3_early_platform_setup(u_register_t arg0, u_register_t arg1,
 	bl_mem_params_node_t *params = s32g_bl2_mem_params_descs;
 	struct s32g_ssram_mailbox *ssram_mb = (void *)BL31SSRAM_MAILBOX;
 
+	NOTICE("##### TOGG_TEST function entery\n");fflush();
 	reset_cause = get_reset_cause();
 	clear_reset_cause();
 	if ((reset_cause == CAUSE_WAKEUP_DURING_STANDBY) &&
 	    !ssram_mb->short_boot) {
 		/* Trampoline to bl31_warm_entrypoint */
+		NOTICE("##### TOGG_TEST normal run\n");fflush();
 		resume_bl31(ssram_mb);
+		NOTICE("##### TOGG_TEST some error\n");fflush();
 		panic();
 	}
 
 	s32_early_plat_init(false);
 	console_s32_register();
 	s32_io_setup();
-
+	NOTICE("##### TOGG_TEST reset cause\n");fflush();
 	NOTICE("Reset status: %s\n", get_reset_cause_str(reset_cause));
 
 	add_fip_img_to_mem_params_descs(params, &index);
